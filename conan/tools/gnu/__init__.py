884641dcdc546ab590dd6e6541f010bab7a47662
James Benton <bentonj@vmware.com>
2024-03-05 09:53:29 +0000

    Add PkgConfigCache for reproducing data from original .pc files

--- conan/tools/gnu/__init__.py
+++ conan/tools/gnu/__init__.py
@@ -2,5 +2,6 @@ from conan.tools.gnu.autotools import Autotools
 from conan.tools.gnu.autotoolstoolchain import AutotoolsToolchain
 from conan.tools.gnu.autotoolsdeps import AutotoolsDeps
 from conan.tools.gnu.pkgconfig import PkgConfig
+from conan.tools.gnu.pkgconfigcache import PkgConfigCache
 from conan.tools.gnu.pkgconfigdeps import PkgConfigDeps
 from conan.tools.gnu.makedeps import MakeDeps
